f37fff5897e8a369ee590be04a0a5437f06d9ce0
Dharshini048 <sureshdeva3526@gmail.com>
2024-10-20 01:01:18 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+var options = {
+    strings: ["Frontend Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+};
+
+var typed = new Typed("#typed-text", options);
